604b9d3b010e33d8548efa53d6424c66091e28d1
davidojo1144 <davco1144@gmail.com>
2025-04-15 13:28:22 +0100

    An Online learning platform for Adult that are illitate

--- Learnnow/src/App.jsx
+++ Learnnow/src/App.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Home from "./pages/Home"
+import About from "./pages/About"
+import ContactUs from "./pages/ContactUs"
+import Learn from "./pages/Learn"
+import Login from "./pages/Login"
+import Review from "./pages/Reviews"
+
+
 
 const App = () => {
   return (
-    <div>
-      
+    <div className='overflex-x-hidden '>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/' element={<About/>} />
+      </Routes>
     </div>
   )
 }
