6f51d9b2fde6b809c5d27378403ee449dcd0f05a
davidojo1144 <davco1144@gmail.com>
2025-04-20 00:02:29 +0100

    An Online learning platform for Adult that are illitrate

--- Learnnow/src/components/Footer.jsx
+++ Learnnow/src/components/Footer.jsx
@@ -1,9 +1,38 @@
 import React from 'react'
+import { assets } from '../assets/assets'
 
 const Footer = () => {
   return (
-    <div>
-      
+    <div className='bg-blue-600 mt-20 '>
+      <div className='flex md:flex-row flex-col items-center justify-between container text-white'>
+        <div>
+          <img className='md:w-44 w-32' src={assets.logo} alt="" />
+          <p></p>
+          <div>
+            <div>
+              <img src="" alt="" />
+            </div>
+            <div>
+              <img src="" alt="" />
+            </div>
+            <div>
+              <img src="" alt="" />
+            </div>
+            <div>
+              <img src="" alt="" />
+            </div>
+          </div>
+        </div>
+        <div>
+          <p>Products</p>
+        </div>
+        <div>
+          <p>Useful Links</p>
+        </div>
+        <div>
+          <p>Address</p>
+        </div>
+      </div>
     </div>
   )
 }
