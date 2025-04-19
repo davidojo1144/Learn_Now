9790fe3cf3693279d78017729f836ed61905566e
davidojo1144 <davco1144@gmail.com>
2025-04-19 23:41:10 +0100

    An Online learning platform for Adult that are illitrate

--- Learnnow/src/components/Video.jsx
+++ Learnnow/src/components/Video.jsx
@@ -1,18 +1,35 @@
-import React from 'react'
-import Title from './Title'
-
+import React from 'react';
+import Title from './Title';
+import YouTube from 'react-youtube';
 
 const Video = () => {
+  const opts = {
+    height: '100%',
+    width: '100%',
+    playerVars: {
+      autoplay: 1,
+      mute: 1,
+      loop: 1,
+      playlist: 'KUWn_TJTrnU'
+    },
+  };
+
   return (
-    <div className='container'>
+    <div className='container pb-10'>
       <div>
         <div className='text-center md:text-2xl text-xl'>
-            <Title text1={"Why Learning"} text2={"Is Important"}/>
+          <Title text1={"Why Learning"} text2={"Is Important"}/>
         </div>
-        <video className='w-full rounded' src="https://youtu.be/KUWn_TJTrnU?t=113" controls autoPlay loop></video>
+        <div className="w-full aspect-video rounded overflow-hidden">
+          <YouTube
+            videoId="KUWn_TJTrnU"
+            opts={opts}
+            className="w-full h-full"
+          />
+        </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default Video
+export default Video;
