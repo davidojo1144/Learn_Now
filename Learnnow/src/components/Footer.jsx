69e573858bc5a7ec32273f94f14ba600d2f50b5a
davidojo1144 <davco1144@gmail.com>
2025-04-20 08:43:18 +0100

    An Online learning platform for Adult that are illitrate

--- Learnnow/src/components/Footer.jsx
+++ Learnnow/src/components/Footer.jsx
@@ -4,11 +4,11 @@ import { assets } from '../assets/assets'
 
 const Footer = () => {
   return (
-    <footer className='mt-20 '>
+    <footer className='mt-20 pb-20'>
       <div className='flex md:flex-row flex-col md:items-center md:space-y-0 space-y-10 justify-between container text-white'>
         <div>
           <img className='md:w-44 w-32' src={assets.logo} alt="" />
-          <p>Learning is the key to growth—it empowers <br/>you to adapt, overcome challenges, and unlock new <br/>opportunities. Every skill you gain and every idea you <br/>explore strengthens your future. Knowledge builds <br/>confidence, fuels progress, and keeps your mind sharp. <br/>No matter where life takes you, learning ensures you’re always <br/>moving forward.</p>
+          <p>Learning is the key to growth—it <br/>empowers you to adapt, overcome challenges, <br/>and unlock new opportunities. Every skill you <br/>gain and every idea you explore strengthens <br/>your future. Knowledge builds confidence, fuels <br/>progress, and keeps your mind sharp. <br/>No matter where life takes you, learning <br/>ensures you’re always moving forward.</p>
           <div>
             <div>
               <img src="" alt="" />
