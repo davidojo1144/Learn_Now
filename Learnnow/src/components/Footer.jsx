09ca0de210c73daa3ded3c8046da6a5ccdbd64db
davidojo1144 <davco1144@gmail.com>
2025-04-20 09:18:55 +0100

    An Online learning platform for Adult that are illitrate

--- Learnnow/src/components/Footer.jsx
+++ Learnnow/src/components/Footer.jsx
@@ -4,8 +4,8 @@ import { assets } from '../assets/assets'
 
 const Footer = () => {
   return (
-    <footer className='mt-20 pb-20'>
-      <div className='flex md:flex-row flex-col md:items-center md:space-y-0 space-y-10 justify-between container text-white'>
+    <footer className='mt-20 pb-10'>
+      <div className='flex md:flex-row flex-col md:items-center md:space-y-0 space-y-16 justify-between container text-white'>
         <div>
           <img className='md:w-44 w-32' src={assets.logo} alt="" />
           <p  className='text-sm font-light'>Learning is the key to growth—it <br/>empowers you to adapt, overcome challenges, <br/>and unlock new opportunities. Every skill you <br/>gain and every idea you explore strengthens <br/>your future. Knowledge builds confidence, fuels <br/>progress, and keeps your mind sharp. <br/>No matter where life takes you, learning <br/>ensures you’re always moving forward.</p>
@@ -22,7 +22,7 @@ const Footer = () => {
           </div>
         </div>
         <div>
-          <p className='md:text-xl text-lg pb-20'>Products</p>
+          <p className='md:text-xl text-lg md:pb-20 pb-2'>Products</p>
           <div className='text-sm font-light'>
             <p>Writing & Note-Taking</p>
             <p>Reading & Reference</p>
@@ -34,20 +34,21 @@ const Footer = () => {
           </div>
         </div>
         <div>
-          <p className='md:text-xl text-lg pb-32'>Useful Links</p>
+          <p className='md:text-xl text-lg md:pb-32 pb-2'>Useful Links</p>
           <div className='text-sm font-light'>
-            <p href="#">Blogs</p>
-            <p href="#">Pricing</p>
-            <p href="#">Certifications</p>
-            <p href="#">Reviews</p>
-            <p href="#">Success Stories</p>
+            <p className='cursor-pointer' href="#">Blogs</p>
+            <p className='cursor-pointer' href="#">Pricing</p>
+            <p className='cursor-pointer' href="#">Certifications</p>
+            <p className='cursor-pointer' href="#">Reviews</p>
+            <p className='cursor-pointer' href="#">Success Stories</p>
           </div>
         </div>
         <div>
-          <p className='md:text-xl text-lg pb-40'>Address</p>
+          <p className='md:text-xl text-lg md:pb-40 pb-2'>Address</p>
           <p>145, Mark way <br/>Sabo Yaba <br/> Lagos State, Nigeria.</p>
         </div>
       </div>
+      <p className='text-center pt-10'>All Rights Reserved by © Learn Now 2025</p>
     </footer>
   )
 }
