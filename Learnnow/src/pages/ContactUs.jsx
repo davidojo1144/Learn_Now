f4bff804d63784214e6b747d5cdb4ebc697d9367
davidojo1144 <davco1144@gmail.com>
2025-04-20 11:21:48 +0100

    An Online learning platform for Adult that are illitrate

--- Learnnow/src/pages/ContactUs.jsx
+++ Learnnow/src/pages/ContactUs.jsx
@@ -13,15 +13,22 @@ const ContactUs = () => {
         <div className='text-center md:text-2xl text-xl pb-20'>
           <Title text1={"CONTACT"} text2={"US"}/>
         </div>
-        <div className='flex md:flex-row flex-col'>
+        <div className='flex md:flex-row  justify-between flex-col'>
           <div className='items-center space-y-3'>
             <h1 className='md:text-xl text-sm font-semibold'>Get in touch</h1>
-            <p>Email: {email}</p>
-            <p>Phone: {phone}</p>
-            <p>Stuck on something? Need guidance? <br/>Contact our team—we’re dedicated to making your <br/>learning experience smooth and successful. 
+            <p className="text-sm md:font-medium font-light">Email: {email}</p>
+            <p className="text-sm md:font-medium font-light">Phone: {phone}</p>
+            <p className="text-sm md:font-medium font-light">Stuck on something? Need guidance? <br/>Contact our team—we’re dedicated to making your <br/>learning experience smooth and successful. 
             <br/>Reach out anytime!</p>
           </div>
-          <div></div>
+          <div className='space-y-5'>
+            <div className='flex space-x-5'>
+              <input className='w-full py-1 px-5 border-2 rounded' type="text"  placeholder='Name'/>
+              <input className='w-full py-3 px-5' type="email" placeholder='Email'/>
+            </div>
+            <textarea className='w-full py-10 rounded' name="text" id="" placeholder='Message'></textarea>
+            <button className='py-2 px-8 bg-blue-400 hover:bg-blue-500 rounded-xl text-white'>Send</button>
+          </div>
         </div>
       </div>
     </div>
