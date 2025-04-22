9e37cf52f44ad88102f72761470e5d19fd3a788b
davidojo1144 <davco1144@gmail.com>
2025-04-22 13:36:15 +0100

    An Online learning platform for Adult Literacy

--- Learnnow/src/pages/Login.jsx
+++ Learnnow/src/pages/Login.jsx
@@ -2,10 +2,18 @@ import React, { useState } from 'react'
 
 const Login = () => {
   const [logs, setLogs] = useState("Sign Up")
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const [dateOfBirth, setDateOfBirth] = useState("")
+
+
+  const handleSubmit = (event)=> {
+    event.preventDefault()
+  }
   
   return (
     <div className='container'>
-      <div className='flex flex-col items-center'>
+      <form onSubmit={handleSubmit} className='flex flex-col items-center'>
         <div>
           <p className='prata-regular text-center md:text-2xl text-xl font-medium'>{logs}</p>
         </div>
@@ -14,12 +22,16 @@ const Login = () => {
             className='w-full p-2 border rounded border-gray-400 focus:border-blue-400 hover:border-blue-300 outline-none transition duration-200 '
             type="email" 
             placeholder='Email' 
+            value={email}
+            onChange={(event)=>setEmail(event.target.value)}
             required
           />
           <input 
             className='w-full p-2 border border-gray-400 rounded focus:border-blue-400 hover:border-blue-300 outline-none transition duration-200'
             type="password" 
             placeholder='Password' 
+            value={password}
+            onChange={(event)=>setPassword(event.target.value)}
             required
           />
           {
@@ -29,6 +41,8 @@ const Login = () => {
             className='w-full p-2 border border-gray-400 rounded focus:border-blue-400 hover:border-blue-300 outline-none transition duration-200'
             type="text" 
             placeholder='D-O-B' 
+            value={dateOfBirth}
+            onChange={(event)=>setDateOfBirth(event.target.value)}
             required
           />
           : ""
@@ -39,10 +53,10 @@ const Login = () => {
           <p className='text-sm cursor-pointer'>{logs === "Login" ? "Forgot password" : "Already have an account?"}</p>
           <p onClick={()=> setLogs(logs === "Sign Up" ? "Login" : "Sign Up")} className='text-sm cursor-pointer'>{logs === "Sign Up" ? "Login" : "Sign Up"}</p>
         </div>
-        <button className='mt-6 px-6 py-2 bg-blue-500 text-white rounded hover:bg-blue-600'>
+        <button type='submit' className='mt-6 px-6 py-2 bg-blue-400 text-white rounded hover:bg-blue-500'>
           {logs}
         </button>
-      </div>
+      </form>
     </div>
   )
 }
