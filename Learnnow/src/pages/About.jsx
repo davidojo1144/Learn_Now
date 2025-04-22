dc2eb2c065d61efef79e1fc4efa23918b30d5470
davidojo1144 <davco1144@gmail.com>
2025-04-22 10:52:16 +0100

    An Online learning platform for Adult Literacy

--- Learnnow/src/pages/About.jsx
+++ Learnnow/src/pages/About.jsx
@@ -5,7 +5,7 @@ import { assets } from '../assets/assets'
 const About = () => {
   return (
     <div className='container'>
-       <div className='md:text-2xl text-xl text-center pb-10 md:pb-16'>
+       <div className='md:text-2xl text-xl text-center pb-10 '>
           <Title text1={"ABOUT"} text2={"US"} />
         </div>
       <div className='flex md:flex-row flex-col justify-center gap-16'>
@@ -17,6 +17,27 @@ const About = () => {
             <p className='prata-regular md:text-xl text-lg md:font-semibold font-medium'>Our Mission</p>
             <p>At Learn Now, we believe that learning is the key to growth, empowerment, and a brighter future. Our mission is to make education accessible, engaging, and meaningful for everyone. We strive to foster curiosity, inspire lifelong learning, and equip individuals with the knowledge and skills they need to thrive in an ever-changing world.</p>
           </div>
+        </div>
+      </div>
+      <div>
+        <div className='md:text-2xl text-xl  md:pt-16'>
+          <Title text1={"WHY"} text2={"CHOOSE US"}/>
+        </div>
+      <div>
+        <div className='flex flex-col md:flex-row text-sm mb-20'>
+        <div className='border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5'>
+          <b>Quality Assurance:</b>
+          <p className='text-gray-600'>At Learn Now, we are committed to delivering excellence in education through rigorous quality assurance. Our systematic approach ensures that every learning experience is effective, engaging, and aligned with the highest standards.</p>
+        </div>
+        <div className='border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5'>
+          <b>Concenience:</b>
+          <p className='text-gray-600'>At Learn Now, we believe education should fit your life—not the other way around. That’s why we’ve designed learning to be flexible, accessible, and hassle-free.</p>
+        </div>
+        <div className='border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5'>
+          <b>Exceptional Customer Service:</b>
+          <p className='text-gray-600'>At Learn Now, your success is our priority. We don’t just provide courses—we provide support that makes learning effortless and enjoyable.</p>
+        </div>
+      </div>
         </div>
       </div>
     </div>
